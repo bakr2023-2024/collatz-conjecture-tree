ef4d59560be59e447a79091aeb9535f47b3b593b
bakr2023-2024 <bakr2023.2024@gmail.com>
2026-05-03 05:44:20 +0300

    precomputed lines segments for each branch

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 #include <vector>
 #define _USE_MATH_DEFINES
 #include <cmath>
-#include <iostream>
 using namespace std;
+using Segment = pair<Vector2, Vector2>;
 vector<int> computeSequence(int n)
 {
     vector<int> seq;
@@ -16,28 +16,36 @@ vector<int> computeSequence(int n)
 }
 int main(void)
 {
+    int branches = 2000;
     float sw = 960, sh = 720;
     float turn = 0.08f;
     // movement vector represented as <0,-L> where L is line length, to move upwards
     float len = -7.0f;
+    vector<Segment> segments;
+    segments.reserve(10 * branches);
+    for (int n = 2; n <= branches; n++)
+    {
+        Vector2 pos{sw / 4, sh};
+        float θ = -M_PI_2;
+        vector<int> seq = computeSequence(n);
+        for (int i = 0; i < seq.size() - 1; i++)
+        {
+            θ = 2 * seq[i] == seq[i + 1] ? θ + 2 * turn : θ - turn;
+            // apply rotation matrix to movement vector and add to pos vector to get newPos
+            Vector2 newPos{pos.x - sinf(θ) * len, pos.y + cosf(θ) * len};
+            segments.emplace_back(pos, newPos);
+            pos = newPos;
+        }
+    }
     InitWindow(sw, sh, "Collatz");
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(BLACK);
-        for (int n = 2; n <= 2500; n++)
+        for (auto &[pos, newPos] : segments)
         {
-            vector<int> seq = computeSequence(n);
-            Vector2 pos{sw / 4, sh * 0.9f};
-            float θ = -M_PI_2;
-            for (int i = 0; i < seq.size() - 1; i++)
-            {
-                θ = 2 * seq[i] == seq[i + 1] ? θ + 2 * turn : θ - turn;
-                // apply rotation matrix to movement vector and add to pos vector to get newPos
-                Vector2 newPos{pos.x - sinf(θ) * len, pos.y + cosf(θ) * len};
-                DrawLineV(pos, newPos, WHITE);
-                pos = newPos;
-            }
+
+            DrawLineV(pos, newPos, WHITE);
         }
         EndDrawing();
     }
