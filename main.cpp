d660bf5db0a6c72c11cc6d6fcc2bd904d816cffb
bakr2023-2024 <bakr2023.2024@gmail.com>
2026-05-03 04:35:03 +0300

    computed the collatz sequence of n as a vector to iterate

--- main.cpp
+++ main.cpp
@@ -1,4 +1,19 @@
 #include <raylib.h>
+#include <vector>
+#define _USE_MATH_DEFINES
+#include <cmath>
+using namespace std;
+vector<int> computeSequence(int n)
+{
+    vector<int> seq;
+    seq.push_back(1);
+    while (n > 1)
+    {
+        seq.push_back(n);
+        n = n % 2 == 0 ? n / 2 : (3 * n + 1) / 2;
+    };
+    return seq;
+}
 int main(void)
 {
     int sw = 960, sh = 720;
@@ -6,7 +21,12 @@ int main(void)
     while (!WindowShouldClose())
     {
         BeginDrawing();
-        ClearBackground(BLUE);
+        ClearBackground(BLACK);
+        for (int n = 2; n <= 2500; n++)
+        {
+            vector<int> seq = computeSequence(n);
+            
+        }
         EndDrawing();
     }
     CloseWindow();
