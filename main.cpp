2c9923f9bb4bcf030306f2344fb4e3d0682b138b
bakr2023-2024 <bakr2023.2024@gmail.com>
2026-05-03 04:56:09 +0300

    fixed bug where pos was not being set to newPos after drawing a line

--- main.cpp
+++ main.cpp
@@ -16,7 +16,10 @@ vector<int> computeSequence(int n)
 }
 int main(void)
 {
-    int sw = 960, sh = 720;
+    float sw = 960, sh = 720;
+    float turn = 0.08f;
+    // movement vector represented as <0,-L> where L is line length, to move upwards
+    float len = -5.0f;
     InitWindow(sw, sh, "Collatz");
     while (!WindowShouldClose())
     {
@@ -25,7 +28,16 @@ int main(void)
         for (int n = 2; n <= 2500; n++)
         {
             vector<int> seq = computeSequence(n);
-            
+            Vector2 pos{sw / 2, sh / 2};
+            float θ = 0.0f;
+            for (int i = 1; i < seq.size() - 1; i++)
+            {
+                θ = 2 * seq[i] == seq[i + 1] ? θ + 2 * turn : θ - turn;
+                // apply rotation matrix to movement vector and add to pos vector to get newPos
+                Vector2 newPos{pos.x - sinf(θ) * len, pos.y + cosf(θ) * len};
+                DrawLineV(pos, newPos, WHITE);
+                pos = newPos;
+            }
         }
         EndDrawing();
     }
